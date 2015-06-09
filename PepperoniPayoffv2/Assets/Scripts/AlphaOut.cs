f14d3697614440d9cc37632707c4557d815e2259
Ralonzo <ruben.nunez@igt.com>
2015-06-09 09:30:56 -0400

    Version 1 Pepperoni Payoff

---
+++ PepperoniPayoffv2/Assets/Scripts/AlphaOut.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AlphaOut : MonoBehaviour {
+	public Image black;
+	//public Color startingColor;
+	public GameObject pepperoni;
+	public GameObject payoff;
+	public GameObject endPepperoni;
+	public GameObject endPayoff;
+
+	float duration = 2.5f; // This will be your time in seconds.
+	float smoothness = 0.02f; // This will determine the smoothness of the lerp. Smaller values are smoother. Really it's the time between updates.
+
+	void Start()
+	{
+
+	}
+	
+	IEnumerator LerpColor()
+	{
+		float progress = 0; //This float will serve as the 3rd parameter of the lerp function.
+		float increment = smoothness/duration; //The amount of change to apply.
+		
+		while(progress < 1)
+		{
+			black.color = Color.Lerp(Color.black, Color.clear, progress);
+			pepperoni.transform.position = Vector2.Lerp (pepperoni.transform.position, endPepperoni.transform.position, progress);
+			payoff.transform.position = Vector2.Lerp (payoff.transform.position, endPayoff.transform.position, progress);
+			progress += increment;
+			yield return new WaitForSeconds(smoothness);
+		}
+		return true;
+	}
+
+	public void FadeIn(){
+		StartCoroutine("LerpColor");
+	}
+}
