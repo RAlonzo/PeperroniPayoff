727360a080e478fcbb5fd25b8aa0dd1f81ab90ab
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-07-06 09:26:37 -0400

    started game states
    dfgadfg

---
+++ PepperoniPayoffv2/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	enum GAME_STATES{menu,game,end};
+	GAME_STATES currentState;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(currentState);
+		currentState = GAME_STATES.menu;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{
+		currentState = GAME_STATES.game;
+		Debug.Log(currentState);
+	}
+
+	public void EndScreen()
+	{
+		currentState = GAME_STATES.end;
+		Debug.Log(currentState);
+	}
+}
