a4808ed0e6d865288f53495bfbbb999985041f84
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-07-08 14:52:34 -0400

    Cleaned Up Work Space

---
+++ PepperoniPayoffv2/Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebCamTexture webCam = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webCam;
+		webCam.Play();
+	}
+ 	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
