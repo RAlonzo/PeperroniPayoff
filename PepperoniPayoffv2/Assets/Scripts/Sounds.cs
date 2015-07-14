214c3f8ba5d47f83333a19a9a6684adb5a5e3899
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-07-14 16:52:28 -0400

    BIG PUSH
    new ui

--- PepperoniPayoffv2/Assets/Scripts/Sounds.cs
+++ PepperoniPayoffv2/Assets/Scripts/Sounds.cs
@@ -14,6 +14,13 @@ public class Sounds : MonoBehaviour {
 	public AudioListener gameSounds;
 
 
+	public Button soundButton;
+
+	public Sprite textureUnmuted;
+
+	public Sprite textureMuted;
+
+
 
 	public void activateSound () {
 		background.Play ();
@@ -35,9 +42,12 @@ public class Sounds : MonoBehaviour {
 		if(AudioListener.volume == 1)
 		{
 			AudioListener.volume = 0;
+			soundButton.GetComponent<Image>().sprite = textureMuted;
 		}else
 		{
 			AudioListener.volume = 1;
+			soundButton.GetComponent<Image>().sprite = textureUnmuted;
+			
 		}
 	}
 
