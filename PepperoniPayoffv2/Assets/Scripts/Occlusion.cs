3c15ef8048a673c3db5076397bdbde2ff3ccca9f
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-10-02 12:54:32 -0400

    culling objects

---
+++ PepperoniPayoffv2/Assets/Scripts/Occlusion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Occlusion : MonoBehaviour {
+
+    public List<GameObject> CulledObjects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CullObjects(float sec)
+    {
+        StartCoroutine(WaitAndCull(sec));
+    }
+
+
+    IEnumerator WaitAndCull(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        foreach (GameObject cullableObj in CulledObjects)
+        {
+            cullableObj.SetActive(false);
+        }
+        yield return 0;
+    }
+}
